e23a524401f5db75cc35de6d449a4a75b99a456d
bansi43 <bansiundhad34@gmail.com>
2024-07-01 11:50:36 +0530

    cg

---
+++ frindfuntion2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class large{
+
+
+ friend void number(large a);
+    int x,y; 
+
+     public:
+    void getdata(int a, int b){
+        x=a, y=b;
+    }
+};
+
+   void number(large a)
+   {
+        if(a.x>a.y){
+            cout<<"x is large "<<endl;
+            
+        }
+        else{
+            cout<<"y is large "<<endl;
+        }
+
+    }
+
+
+int main ()
+{
+    large t;
+    t.getdata (20,10);
+    number(t);
+
+    return 0;
+}
